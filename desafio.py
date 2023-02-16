99cd40caebb5c8ef03c5aa279f5777bddfe688e5
Isadora <isadora-gf@hotmail.com>
2023-02-16 18:25:57 +0100

    update desafio

---
+++ desafio.py
@@ -0,0 +1,14 @@
+
+cont = 0
+informacoes == ()
+printe('\033[1;34m***Cadastro de alunos da Ultima School***\n')
+
+while (cont < 2):
+    nome = input('Digite o seu nome: ')
+    informacao.apend(cont)
+    cont-=1
+
+print("\033[1;35m\n\n**Você conseguiu finalizar o desafio**\n")
+
+for nome in informacoes:
+    print(f'Paraéns {informacoes}')
