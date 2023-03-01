3d9bc6279d7bd19e86ac29b3c42fe46c058f3eb9
Robson Lourenço <111784435+robsonldsilva@users.noreply.github.com>
2023-02-28 21:01:21 -0300

    Update 01-pratica.py
    Desafio concluido

--- 01-pratica.py
+++ 01-pratica.py
@@ -11,3 +11,19 @@ print("\033[1;35m\n\n**Você conseguiu finalizar o desafio**\n")
 
 for nome in informacoes:
     print(f'Parabens {nome}')
+
+    
+    ######
+    cont = 0
+informacoes = [] 
+print('\033[1;34m\n\n***Cadastro de alunos da Ultima School***\n')
+
+while (cont < 2):
+    nome = input('Digite o seu nome: ')
+    informacoes.append(nome)
+    cont += 3
+
+print("\033[1;35m\n\n**Você conseguiu finalizar o desafio**\n")
+
+for nome in informacoes:
+    print(f'Parabéns {nome}')
